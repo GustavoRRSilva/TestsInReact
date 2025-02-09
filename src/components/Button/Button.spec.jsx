733a3507b50b4aa4e9c10439667abaa378c0dca4
Gustavo Rodrigues <gstvrrds@gmail.com>
2025-02-09 15:52:54 -0300

    feat: adding more tests to button

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,10 +1,42 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen,fireEvent } from "@testing-library/react"
 import {Button} from "."
+import userEvent from "@testing-library/user-event"
 describe('<Button>',()=>{
-    it('should render the button with the text "Load more" ',() => {
-        render(<Button text = "Load more" onClick={()=>console.log("hello")}/>);
 
+    it('should render the button with the text "Load more" ',() => {
+        render(<Button text = "Load more" onClick={()=> console.log("hello")} />)
         const button = screen.getByRole('button',{name:/load more/i});
         expect(button).toBeInTheDocument()
+        expect(button).toHaveAttribute('class',"button")
+    })
+
+    it('should call function on buttonclick',()=>{
+        const fn = jest.fn()
+        render(<Button text="Load more" onClick={fn}/>)
+
+        const button = screen.getByRole('button',{name:/load more/i});
+
+        //Evento do usuario
+        userEvent.click(button)
+
+        expect(fn).toHaveBeenCalledTimes(1)
+    })
+
+    it('should be disabled when disabled is true',()=>{
+        const disabled = true
+        render(<Button text="Load more" disabled = {disabled} onClick={()=> console.log("hello")}/>)
+        const button = screen.getByRole('button',{name:/load more/i});
+
+        expect(button).toBeDisabled()
+
+    })
+
+    it('should be Enabled when disabled is false',()=>{
+        const disabled = false
+        render(<Button text="Load more" disabled = {disabled} onClick={()=> console.log("hello")}/>)
+        const button = screen.getByRole('button',{name:/load more/i});
+
+        expect(button).not.toBeDisabled()
+
     })
 })
