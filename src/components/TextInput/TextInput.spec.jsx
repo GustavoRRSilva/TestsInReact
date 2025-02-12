5333a4097df54af9400fa383ec073276f0ae1bd5
Gustavo Rodrigues <gstvrrds@gmail.com>
2025-02-12 19:24:21 -0300

    feat: adding tests to project

--- src/components/TextInput/TextInput.spec.jsx
+++ src/components/TextInput/TextInput.spec.jsx
@@ -1,8 +1,20 @@
 import { render,screen } from "@testing-library/react"
-import TextInput from "./index.jsx"
+import {TextInput} from "."
 
 describe('<TextInput>',()=>{
-    it('',()=>{
-        render(TextInput)
+    
+    it('Should have a value of searchvalue',()=>{
+        const fn = jest.fn()
+        render(<TextInput handleChange={fn} searchValue={'testando'}/>)
+    })
+
+    it('Should call handlechange function on each key pressed',()=>{
+        const fn = jest.fn()
+        render(<TextInput handleChange={fn} searchValue={'testando'}/>)
+
+        const input = screen.getAllByPlaceholderText(/type your search/i)[0];
+        expect(input).toBeInTheDocument()
+
+        expect(input.value).toBe('testando')
     })
 })
